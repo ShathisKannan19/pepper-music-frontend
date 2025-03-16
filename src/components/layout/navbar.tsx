856a1ab7ae412b37540fa08e08c26e0675052a5e
shadowctrl <admin@shadowctrl.me>
2025-03-16 23:44:24 +0530

    Added clickable for pepper navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -10,7 +10,9 @@ interface Props {}
 const Navbar: NextPage<Props> = ({}) => {
 	return (
 		<header className="flex items-center justify-between px-8 py-4 bg-black border-b border-gray-700 text-white">
-			<h1 className="text-2xl font-bold">Pepper</h1>
+			<Link href={'/'}>
+				<h1 className="text-2xl font-bold">Pepper</h1>
+			</Link>
 			<nav>
 				<ul className="flex space-x-6">
 					{menuItems.map((item: MenuItemType) => (
